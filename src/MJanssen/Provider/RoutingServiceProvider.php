33e3ca2b35ad59b800aef4ee648be6d4fdd7d36f
Marco Janssen <marco.janssen@outlook.com>
2013-09-27 15:56:09 +0200

    Added configurable app routing key, refactored getController

--- src/MJanssen/Provider/RoutingServiceProvider.php
+++ src/MJanssen/Provider/RoutingServiceProvider.php
@@ -13,13 +13,26 @@ use Silex\ServiceProviderInterface;
 class RoutingServiceProvider implements ServiceProviderInterface
 {
     /**
+     * @var
+     */
+    protected $appRoutingKey;
+
+    /**
+     * @param string $appRoutingKey
+     */
+    public function __construct($appRoutingKey = 'config.routes')
+    {
+        $this->appRoutingKey = $appRoutingKey;
+    }
+
+    /**
      * @param Application $app
      */
     public function register(Application $app)
     {
-        if(isset($app['config.routes'])) {
-            if(is_array($app['config.routes'])) {
-                $this->addRoutes($app, $app['config.routes']);
+        if(isset($app[$this->appRoutingKey])) {
+            if(is_array($app[$this->appRoutingKey])) {
+                $this->addRoutes($app, $app[$this->appRoutingKey]);
             } else {
                 throw new InvalidArgumentException('config.routes must be of type Array');
             }
@@ -61,7 +74,10 @@ class RoutingServiceProvider implements ServiceProviderInterface
         }
 
         if(!is_array($route['method'])) {
-            throw new InvalidArgumentException('Method is not of type Array');
+            throw new InvalidArgumentException(sprintf(
+                'Method is not of type Array (%s)',
+                gettype($route['method'])
+            ));
         }
 
         if(!isset($route['controller'])) {
@@ -109,21 +125,12 @@ class RoutingServiceProvider implements ServiceProviderInterface
      */
     protected function getController(Application $app, $route)
     {
-        if('get' === $route['method']) {
-            return $app->get($route['pattern'], $route['controller']);
+        $availableMethods = array('get', 'put', 'post', 'delete');
+        if(!in_array($route['method'], $availableMethods)) {
+            throw new InvalidArgumentException('Method is not valid, only the following methods are allowed: get, put, post, delete');
         }
 
-        if('post' === $route['method']) {
-            return $app->post($route['pattern'], $route['controller']);
-        }
-
-        if('put' === $route['method']) {
-            return $app->put($route['pattern'], $route['controller']);
-        }
-
-        if('delete' === $route['method']) {
-            return $app->delete($route['pattern'], $route['controller']);
-        }
+        return call_user_func_array(array($app, $route['method']), array($route['pattern'], $route['controller']));
     }
 
     /**
